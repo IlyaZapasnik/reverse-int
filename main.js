7b9656cac32c3c9a06e829ab2d76ca24351c3535
Ilya <55zappa55@gmail.com>
2023-08-13 18:09:37 +0200

    reverse function

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-module.exports = function reverse (n) {
+function reverse (n) {
     if (n == 0) {
         return n;
     } else if (n < 0) {
         let a = Math.abs(n);
-        return (Array.from(String(a)).reverse().join(''));
+        return -(Array.from(String(a)).reverse().join(''));
     }
     if (n != NaN) {
         return +(Array.from(String(n)).reverse().join(''));
     }
-    
 }
+
+console.log(reverse(-123));
+
